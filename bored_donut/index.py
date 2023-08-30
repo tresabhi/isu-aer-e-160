ad90b444a1d8da2e1bd7b671ab0a418394697062
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2023-08-30 11:00:53 -0500

    fix: spinning donut

--- bored_donut/index.py
+++ bored_donut/index.py
@@ -5,7 +5,7 @@ SIZE = 35
 OUTER_RADIUS = int(SIZE / 2)
 INNER_RADIUS = int(OUTER_RADIUS / 4)
 
-index = 0
+index = 1
 
 while True:
   os.system('cls')
@@ -14,7 +14,7 @@ while True:
     row = ''
 
     for x in range(int(-SIZE / 2), int(SIZE / 2)):
-      hypot = math.hypot(y, x * math.sin(index * math.pi / 180))
+      hypot = math.hypot(y, x / math.sin(index * math.pi / 180))
       row += '##' if hypot <= OUTER_RADIUS and hypot >= INNER_RADIUS else '  '
 
     print(row)
